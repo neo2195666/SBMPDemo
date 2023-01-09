f8dc402a976212fdc20d3555ad4ed7dc9b98ef73
neo2195357 <neo2195357@gmail.com>
2023-01-09 08:29:34 +0800

    update

--- src/main/java/com/example/sbmpdemo/services/Impl/UserServiceImpl.java
+++ src/main/java/com/example/sbmpdemo/services/Impl/UserServiceImpl.java
@@ -6,9 +6,10 @@ import com.example.sbmpdemo.pojo.User;
 import com.example.sbmpdemo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Component
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
